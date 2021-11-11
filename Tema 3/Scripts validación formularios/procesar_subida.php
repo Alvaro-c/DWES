69c7345d3a8a4f540f1ebadeb66b21c9af00573d
Your Name <you@example.com>
2021-11-11 18:37:01 +0100

    visto script de subir ficheros

--- Tema 3/Scripts validación formularios/procesar_subida.php
+++ Tema 3/Scripts validación formularios/procesar_subida.php
@@ -1,14 +1,14 @@
 <?php
-	$tam = $_FILES["fichero"]["size"];
-	if($tam > 256 *1024){
-		echo "<br>Demasiado grande";
-		return;
-	}
-	echo "Nombre del fichero: " . $_FILES["fichero"]["name"];
-	echo "<br>Nombre temporal del fichero en sel servidor: " . $_FILES["fichero"]["tmp_name"];	
-	$res = move_uploaded_file($_FILES["fichero"]["tmp_name"],"subidos/" . $_FILES["fichero"]["name"]);
-    if($res){
-		echo "<br>Fichero guardado";
-    } else {
-        echo "<br>Error";
-    }
+$tam = $_FILES["fichero"]["size"];
+if ($tam > 256 * 1024) {
+	echo "<br>Demasiado grande";
+	return;
+}
+echo "Nombre del fichero: " . $_FILES["fichero"]["name"];
+echo "<br>Nombre temporal del fichero en sel servidor: " . $_FILES["fichero"]["tmp_name"];
+$res = move_uploaded_file($_FILES["fichero"]["tmp_name"], "subidos/" . $_FILES["fichero"]["name"]);
+if ($res) {
+	echo "<br>Fichero guardado";
+} else {
+	echo "<br>Error";
+}
