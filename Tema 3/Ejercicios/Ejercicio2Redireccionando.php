15b0243a345c3e8df4ad00776e6c3da0e63e28f6
Your Name <you@example.com>
2021-11-11 18:28:44 +0100

    ejercicio2

--- Tema 3/Ejercicios/Ejercicio2Redireccionando.php
+++ Tema 3/Ejercicios/Ejercicio2Redireccionando.php
@@ -2,3 +2,6 @@
 echo "Redireccionado desde: ". $_SERVER["HTTP_REFERER"];
 echo "<p> Usuario: " . $_GET['usuario'] . "</p>";
 echo "<p> Contraseña: " . $_GET['clave'] . "</p>";
+
+// La variable POST en este archivo no contiene nada. 
+// Si queremos recuperar esa información tendremos que llamar a la BBDD o a la variable sesión
